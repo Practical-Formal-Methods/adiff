79ba4bc0c2edc2785ae55ac0bf4f20e761d9edd6
Christian Klinger <io@klinch.de>
2018-03-12 15:09:50 +0100

    removed duplication.

--- src/Verifier/Ultimate.hs
+++ src/Verifier/Ultimate.hs
@@ -1,24 +1,24 @@
 module Verifier.Ultimate(uautomizer, utaipan) where
 
-import Verifier.Util
+import           Verifier.Util
 
 uautomizer :: Verifier
 uautomizer = def { verifierName = "uautomizer", execute = run, version = uautomizerVersion}
-  where run fn = withSpec reachSafety $ \spec -> do
-            let cmd = "Automizer.py --architecture 32bit --file " ++ fn ++ " --spec " ++ spec
-            putStrLn cmd
-            (exitCode, out, _) <- readCreateProcessWithExitCode (shell cmd) ""
-            let lastLine = last $ lines out
-            case (exitCode, lastLine) of
-                (ExitSuccess,"TRUE")  -> return VerificationSuccessful
-                (ExitSuccess,"FALSE") -> return VerificationFailed
-                _                     -> return VerificationResultUnknown
+  where run fn = withSpec reachSafety $ \spec ->
+            runUltimate $ "Automizer.py --architecture 32bit --file " ++ fn ++ " --spec " ++ spec
+
         uautomizerVersion = Just . head . lines <$> readCreateProcess (shell "Automizer.py --version") ""
 
 utaipan :: Verifier
 utaipan = def { verifierName = "utaipan", execute = run, version = uautomizerVersion}
-  where run fn = withSpec reachSafety $ \spec -> do
-            let cmd = "Taipan.py --architecture 32bit --file " ++ fn ++ " --spec " ++ spec
+  where run fn = withSpec reachSafety $ \spec ->
+            runUltimate $ "Taipan.py --architecture 32bit --file " ++ fn ++ " --spec " ++ spec
+
+        uautomizerVersion = Just . head . lines <$> readCreateProcess (shell "Taipan.py --version") ""
+
+
+runUltimate :: String -> IO VerifierResult
+runUltimate cmd = do
             putStrLn cmd
             (exitCode, out, _) <- readCreateProcessWithExitCode (shell cmd) ""
             let lastLine = last $ lines out
@@ -26,5 +26,4 @@ utaipan = def { verifierName = "utaipan", execute = run, version = uautomizerVer
                 (ExitSuccess,"TRUE")  -> return VerificationSuccessful
                 (ExitSuccess,"FALSE") -> return VerificationFailed
                 _                     -> return VerificationResultUnknown
-        uautomizerVersion = Just . head . lines <$> readCreateProcess (shell "Taipan.py --version") ""
-  
+
