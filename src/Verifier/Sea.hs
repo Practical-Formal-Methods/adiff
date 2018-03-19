9fb099c24ab4318a0b573ac2e2cbc6df23d1fe5c
Christian Klinger <io@klinch.de>
2018-03-19 20:50:48 +0100

    sea needs to be called from the directory it resides in.

--- src/Verifier/Sea.hs
+++ src/Verifier/Sea.hs
@@ -1,6 +1,5 @@
 module Verifier.Sea(seahorn) where
 
-import           Data.List     (isInfixOf)
 import           Verifier.Util
 
 seahorn :: Verifier
@@ -8,12 +7,16 @@ seahorn = def { verifierName = "seahorn"
               , execute = runSeahorn
               }
 
+-- | Apparently, the script "sea" has to be run from the directory it is installed in.
+-- At the moment, the path /verifiers/seahorn/bin is hard coded. TODO: Use which or similar to change cwd based on that.
 runSeahorn :: FilePath -> IO (VerifierResult, Timing)
-runSeahorn fn = withSpec reachSafety $ \sp -> do
-  let cmd = shell $ "sea --spec " ++ sp  ++ " " ++ fn
+runSeahorn fn = do
+  let cmd = (shell $ "./sea pf "  ++ fn) {cwd = Just "/verifiers/seahorn/bin"}
+  print cmd
   (_, out, timing) <- execTimed cmd ""
-  let res
-        | "BRUNCH_STAT Result True" `isInfixOf` out = VerificationSuccessful
-        | "BRUNCH_STAT Result FALSE"  `isInfixOf` out = VerificationFailed
-        | otherwise = VerificationResultUnknown
+  let res = case last (lines out) of
+              "sat"   -> VerificationFailed
+              "unsat" -> VerificationSuccessful
+              _       -> VerificationResultUnknown
+  print $ "runSeahorn: " ++ show res
   return (res, timing)
