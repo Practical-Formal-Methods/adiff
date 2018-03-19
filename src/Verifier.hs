7601007837b1a0895181e65f42a82869545facbc
Christian Klinger <io@klinch.de>
2018-03-19 20:51:40 +0100

    added seahorn to list of verifiers.

--- src/Verifier.hs
+++ src/Verifier.hs
@@ -2,15 +2,16 @@ module Verifier (allVerifiers) where
 
 import           Data.List           (sort)
 
-import Types
+import           Types
 
 import           Verifier.Cbmc
 import           Verifier.CpaChecker
 import           Verifier.Klee
+import           Verifier.Sea
 import           Verifier.Ultimate
 import           Verifier.Vim
 
 allVerifiers :: [Verifier]
-allVerifiers = sort [cbmc, cpaChecker, klee, utaipan, uautomizer, vim]
+allVerifiers = sort [cbmc, cpaChecker, klee, utaipan, uautomizer, vim, seahorn]
 
 
