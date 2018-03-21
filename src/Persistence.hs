9cbf6268529d6d8dc0cb7530cd25577af77667f1
Christian Klinger <io@klinch.de>
2018-03-21 12:11:39 +0100

    also save time and memory to sqlite.

--- src/Persistence.hs
+++ src/Persistence.hs
@@ -9,6 +9,7 @@ import qualified Database.SQLite.Simple         as SQL
 import           Database.SQLite.Simple.ToField
 
 import           Data
+import           Timed
 --------------------------------------------------------------------------------
 -- * Persistence
 --------------------------------------------------------------------------------
@@ -19,7 +20,7 @@ initDb mFn = do
   let fn = fromMaybe "vdiff.db" mFn
   conn <- SQL.open fn
   SQL.execute_ conn "CREATE TABLE IF NOT EXISTS programs (code_hash TEXT PRIMARY KEY, origin TEXT, content TEXT)"
-  SQL.execute_ conn "CREATE TABLE IF NOT EXISTS runs (run_id INTEGER PRIMARY KEY, verifier_name TEXT, result TEXT, code_hash TEXT REFERENCES programs )"
+  SQL.execute_ conn "CREATE TABLE IF NOT EXISTS runs (run_id INTEGER PRIMARY KEY, verifier_name TEXT, result TEXT, time INT, memory INT, code_hash TEXT REFERENCES programs )"
   return conn
 
 -- | For things that should be stored into the database
@@ -35,8 +36,12 @@ instance Persistent CProgram where
 
 instance Persistent VerifierRun where
   persist conn run = do
-    let row = [toField (runVerifierName run), toField (verifierResult run), toField (verifierCode run)]
-    SQL.execute conn "INSERT INTO runs(verifier_name,result,code_hash) VALUES (?,?,?)" row
+    let row = [ toField (runVerifierName run)
+              , toField (verifierResult run)
+              , toField (elapsedWall $ verifierTiming run)
+              , toField (maxResidentMemory $ verifierTiming run)
+              , toField (verifierCode run) ]
+    SQL.execute conn "INSERT INTO runs(verifier_name,result,time,memory,code_hash) VALUES (?,?,?,?,?)" row
 
 
 --------------------------------------------------------------------------------
