1ec7660438b53e2f6077c4feecfcac1ebc61e095
Christian Klinger <io@klinch.de>
2018-05-21 16:25:57 +0200

    added test cases.

---
+++ vdiff/assets/test/reads/nested-expressions.c
@@ -0,0 +1,28 @@
+
+int min(int x, int y) {
+  if (x <= y) { return x; }
+  else { return y;}
+}
+
+int max(int x, int y) {
+  if (x >= y) { return x; }
+  else { return y; }
+}
+
+int add(int x, int y) {
+  return (x + y);
+}
+
+int main() {
+
+  int x = 1;
+  int y = 2;
+  int* arr[3];
+
+  if (min(x,y) > 2) {; }
+
+  if (arr[min(1,x)]){}
+
+  if (arr[0] == 1) {}
+}
+
