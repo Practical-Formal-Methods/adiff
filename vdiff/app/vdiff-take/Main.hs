fecedd8fc10a490bad784ce1b6383eb848da38f2
Christian Klinger <io@klinch.de>
2018-06-01 23:15:42 +0200

    added memoization for vdiff-take.

--- vdiff/app/vdiff-take/Main.hs
+++ vdiff/app/vdiff-take/Main.hs
@@ -4,6 +4,7 @@
 import           Control.Monad
 import           Data.List
 import qualified Data.Set              as Set
+import qualified Data.Map as Map
 import           Options.Applicative
 import           RIO
 import           System.Directory
@@ -48,6 +49,13 @@ filterFiles = filter flt
 
 data Finished = Finished
 
+data TakeEnv = TakeEnv
+  { logFunction :: LogFunc
+  , memostore :: IORef (Map FilePath Bool)
+  }
+instance HasLogFunc TakeEnv where
+  logFuncL = lens logFunction (\env l -> env {logFunction = l})
+
 main :: IO ()
 main = do
   parameters <- execParser opts
@@ -55,44 +63,60 @@ main = do
   RIO.hSetBuffering stdout LineBuffering
   logOptions <- logOptionsHandle stderr True
   let logOptions' = setLogMinLevel LevelInfo logOptions
-  withLogFunc logOptions' $ \logger -> runRIO logger $ do
-    logInfo "starting vdiff-take"
-    folders <- (subfolders "." >>= shuffleM)
-    filesRef <- newIORef Set.empty
-    forM_ (cycle folders) $ \folder -> do
-      files <- readIORef filesRef
-      -- abort when we have files
-      when (Set.size files >= numFiles parameters) $ liftIO exitSuccess
-      -- try to pick a file
-      pickFile folder >>= \case
-        Nothing -> logInfo "no file found"
-        Just f -> do
-          logInfo $ "found file: " <> display (tshow f)
-          when (not (Set.member f files)) $ do
-              -- store the file in the set
-              modifyIORef filesRef (Set.insert f)
-              -- write the file to stdout
-              liftIO $ putStrLn f
-
+  withLogFunc logOptions' $ \logger -> do
+    memostoreRef <- newIORef (Map.empty)
+    let env = TakeEnv logger memostoreRef
+    runRIO env $ do
+      logInfo "starting vdiff-take"
+      folders <- (subfolders "." >>= shuffleM)
+      filesRef <- newIORef Set.empty
+      forM_ (cycle folders) $ \folder -> do
+        files <- readIORef filesRef
+        -- abort when we have files
+        when (Set.size files >= numFiles parameters) $ liftIO exitSuccess
+        -- try to pick a file
+        pickFile folder >>= \case
+          Nothing -> logInfo "no file found"
+          Just f -> do
+            logInfo $ "found file: " <> display (tshow f)
+            when (not (Set.member f files)) $ do
+                -- store the file in the set
+                modifyIORef filesRef (Set.insert f)
+                -- write the file to stdout
+                liftIO $ putStrLn f
 
 subfolders :: (MonadIO m, MonadReader env m, HasLogFunc env) => FilePath -> m [FilePath]
 subfolders dir = do
   contents <- liftIO $ listDirectory dir
   filterM (liftIO . doesDirectoryExist) contents
 
-pickFile :: (HasLogFunc env) => FilePath -> RIO env (Maybe FilePath)
+pickFile :: FilePath -> RIO TakeEnv (Maybe FilePath)
 pickFile dir = do
   logInfo $ "trying to pick file from " <> display (tshow dir)
   contents <- filterFiles . filter (".i" `isSuffixOf`) . map (\fn -> dir ++ "/" ++ fn) <$> liftIO (listDirectory dir)
   files <- shuffleM =<< filterM (liftIO.doesFileExist) contents
   takeFirstM testParse files
 
-testParse :: (HasLogFunc env) => FilePath -> RIO env Bool
+-- | this function uses the memostore in the environment to memoize its results
+testParse :: FilePath -> RIO TakeEnv Bool
 testParse file = do
-  res <- liftIO $ runRIO NoLogging $ openCFile file
-  case (force $ prettyp <$> res) of
-    Nothing -> return False
-    Just  _ -> return True
+  storeRef <- asks memostore
+  store <- readIORef storeRef
+  case (Map.lookup file store) of
+    Nothing -> do
+      res <- testParse' file
+      liftIO $ writeIORef storeRef $ Map.insert file res store
+      return res
+    Just res -> return res
+
+
+  where
+    testParse' file = do
+      res <- liftIO $ runRIO NoLogging $ openCFile file
+      case (force $ prettyp <$> res) of
+        Nothing -> return False
+        Just  _ -> return True
+
 
 
 takeFirstM :: (Monad m) => (a -> m Bool) -> [a] -> m (Maybe a)
