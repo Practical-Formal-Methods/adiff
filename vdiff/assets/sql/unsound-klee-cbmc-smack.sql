c8c8b75eb9b18697fe155ac7726b88c0dcc786e3
Christian Klinger <io@klinch.de>
2018-06-01 21:54:12 +0200

    added query for unsound-klee-cbmc-smack

---
+++ vdiff/assets/sql/unsound-klee-cbmc-smack.sql
@@ -0,0 +1,15 @@
+SELECT r.run_id,
+r.verifier_name,
+p.origin,
+r.code_hash,
+(SELECT COUNT(*) FROM runs AS t WHERE r.code_hash = t.code_hash AND result='unsat')  as unsats,
+(SELECT COUNT(*) FROM runs AS t WHERE r.code_hash = t.code_hash AND result='sat')  as sats
+FROM runs r NATURAL JOIN programs p
+WHERE result = 'unsat'
+AND verifier_name <> 'cbmc' AND verifier_name <> 'klee' AND verifier_name <> 'smack'
+AND EXISTS (
+SELECT *
+FROM runs AS q
+WHERE r.code_hash = q.code_hash
+AND result='sat'
+AND (q.verifier_name = 'cbmc' OR  q.verifier_name = 'klee' OR q.verifier_name = 'smack'))
