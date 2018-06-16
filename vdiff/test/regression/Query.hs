cb953ada5b75ee738acf709bb7b0b3222c778924
Christian Klinger <io@klinch.de>
2018-06-16 11:25:34 +0200

    added iteration to regression test.

--- vdiff/test/regression/Query.hs
+++ vdiff/test/regression/Query.hs
@@ -26,12 +26,12 @@ p1 = Program "deadbeaf" "test.c"  "int main(){return 0;}"
 p2 = Program "affe" "test2.c" "int main(char** c){return 1;}"
 
 run1, run2, run3, run4, run5, run6 :: VerifierRun
-run1 = VerifierRun 1 "verifier_uno"  (pk p1) satResult
-run2 = VerifierRun 2 "verifier_due"  (pk p1) unsatResult
-run3 = VerifierRun 3 "verifier_tres" (pk p1) unknownResult
-run4 = VerifierRun 4 "verifier_quat" (pk p1) satResult
-run5 = VerifierRun 5 "verifier_cinq" (pk p2) satResult
-run6 = VerifierRun 6 "verifier_sei"  (pk p1) unsatResult
+run1 = VerifierRun 1 "verifier_uno"  (pk p1) satResult 0
+run2 = VerifierRun 2 "verifier_due"  (pk p1) unsatResult 0
+run3 = VerifierRun 3 "verifier_tres" (pk p1) unknownResult 0
+run4 = VerifierRun 4 "verifier_quat" (pk p1) satResult 0
+run5 = VerifierRun 5 "verifier_cinq" (pk p2) satResult 0
+run6 = VerifierRun 6 "verifier_sei"  (pk p1) unsatResult 0
 
 satResult, unsatResult, unknownResult :: VerifierResult
 satResult     = VerifierResult (Just 1.5) (Just 25343) Sat
