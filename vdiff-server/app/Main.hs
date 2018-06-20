75d911f2d637998b6e44ae0b7034713e5eae1417
Christian Klinger <io@klinch.de>
2018-06-20 22:31:08 +0200

    made the server compile until the generic-queries is merged in.

--- vdiff-server/app/Main.hs
+++ vdiff-server/app/Main.hs
@@ -15,25 +15,17 @@ import           VDiff.Data
 import           VDiff.Persistence
 import           VDiff.Query             as Q
 
-import           VDiff.Server.Controller
-import           VDiff.Server.Prelude
 
 data ServerParameters = ServerParameters
   { port       :: Int
   }
 
 
-parseServerParameters :: Parser ServerParameters
-parseServerParameters = ServerParameters <$> port
-  where port = option auto (long "port" <> value 8080)
-
 
 infos = (progDesc "viewer for vdiff")
 
 main :: IO ()
-main = runVDiffApp parseServerParameters infos $ \sp -> do
-  env <- ask
-  scottyT (port sp) (runRIO env) $ endpoints
+main = error "useless until merge"
 
 
 
