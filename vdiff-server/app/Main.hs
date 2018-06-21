fcd98e461eb8de5b292da2ed6a979241ae0c0ee9
Christian Klinger <io@klinch.de>
2018-06-21 14:24:56 +0200

    re-enabled vdiff-server.

--- vdiff-server/app/Main.hs
+++ vdiff-server/app/Main.hs
@@ -11,22 +11,26 @@ import           System.IO
 import           VDiff.Arguments
 import           VDiff.Data
 import           VDiff.Persistence
-import           VDiff.Query                           as Q
 import           Web.Scotty.Trans
 
+import           VDiff.Server.Controller
+import           VDiff.Server.Prelude
 
 data ServerParameters = ServerParameters
   { port       :: Int
   }
 
 
+parseServerParameters :: Parser ServerParameters
+parseServerParameters = ServerParameters <$> port
+  where port = option auto (long "port" <> value 8080)
+
 
 infos = (progDesc "viewer for vdiff")
 
 main :: IO ()
-main = error "useless until merge"
-
-
-
+main = runVDiffApp parseServerParameters infos $ \sp -> do
+  env <- ask
+  scottyT (port sp) (runRIO env) $ endpoints
 
 
