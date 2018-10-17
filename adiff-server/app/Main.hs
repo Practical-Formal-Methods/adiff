ef5f59d6301372d3351a9925f289eda6ce39d647
Christian Klinger <io@klinch.de>
2018-10-17 21:40:38 +0200

    added --host option (e.g. --host 127.0.0.1 make it listen to localhost only)

--- adiff-server/app/Main.hs
+++ adiff-server/app/Main.hs
@@ -3,13 +3,14 @@
 module Main where
 
 
-import qualified Control.Concurrent.MSemN              as Sema
-import qualified Data.Map                              as Map
-import           Network.Wai.Middleware.StaticEmbedded
-import           System.IO
 import           ADiff.Arguments
 import           ADiff.Data
 import           ADiff.Persistence
+import qualified Control.Concurrent.MSemN              as Sema
+import qualified Data.Map                              as Map
+import qualified Network.Wai.Handler.Warp              as Warp
+import           Network.Wai.Middleware.StaticEmbedded
+import           System.IO
 import           Web.Scotty.Trans
 
 import           ADiff.Application
@@ -20,17 +21,19 @@ import qualified ADiff.Query2                          as Q2
 
 data ServerParameters = ServerParameters
   { port                   :: Int
+  , hostPref               :: Warp.HostPreference
   , forceRecount           :: Bool
   , maxConcurrentVerifiers :: Int
   }
 
 
 parseServerParameters :: Parser ServerParameters
-parseServerParameters = ServerParameters <$> port <*> switchForceRecount <*> maxConcurrent
+parseServerParameters = ServerParameters <$> port <*> host <*> switchForceRecount <*> maxConcurrent
   where
     port = option auto (short 'p' <> long "port" <> value 8080)
     switchForceRecount = switch (long "force-recount" <> help "re-calculates the temporary table that contains all counts of sats/unsats")
     maxConcurrent = option auto (short 'n' <> long "max-concurrent-verifiers" <> value 1)
+    host = option str (long "host" <> value "*")
 
 
 infos = progDesc "runs a webserver to inspect adiff results"
@@ -48,11 +51,12 @@ main = runADiffApp parseServerParameters infos $ \sp -> do
   when (forceRecount sp) Q2.cleanTemporary
 
   -- start server
-  scottyT (port sp) (runRIO env) endpoints
+  let opts = Options 1 (Warp.setHost (hostPref sp) Warp.defaultSettings)
+  scottyOptsT opts (runRIO env) endpoints
 
 
 mkServerEnv :: Sema.MSemN Int -> RIO MainEnv ServerEnv
 mkServerEnv s = do
   menv <- ask
-  tables <- newIORef (Map.empty)
+  tables <- newIORef Map.empty
   return $ ServerEnv menv s tables
